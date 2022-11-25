e1c44db1eb2f066cf25d37f75f067e3ef3e8ace6
Ivan <ivan.inclan.327b@gmail.com>
2022-11-25 16:02:28 -0600

    Decrement & incrementby con redux

--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -5,7 +5,7 @@ import { createSlice } from '@reduxjs/toolkit'
 export const counterSlice = createSlice({
   name: 'counter',
   initialState: {
-    counter: 10
+    counter: 10,
   },
   reducers: {
     increment: (state) => {
@@ -15,8 +15,19 @@ export const counterSlice = createSlice({
       // immutable state based off those changes
       state.counter += 1;
     },
+    decrement: (state) => {
+      state.counter -= 1;
+    },
+    // incrementBy: {
+
+    // }
+    incrementBy : (state, action) => {
+      console.log(action)
+      state.counter += action.payload;
+    },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment } = counterSlice.actions;
+export const { increment, decrement, incrementBy
+ } = counterSlice.actions;
